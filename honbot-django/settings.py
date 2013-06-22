bdb84663353b2ab9528ffdba52b305be517263a6
Scott Cooper <scttcper@gmail.com>
2013-06-22 16:36:37 -0700

    oops

--- honbot-django/settings.py
+++ honbot-django/settings.py
@@ -78,7 +78,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    os.path.join(os.path.abspath(os.path.dirname(os.path.dirname(__file__))), 'src'),
+    os.path.join(os.path.abspath(os.path.dirname(os.path.dirname(__file__))), 'static'),
 
 )
 

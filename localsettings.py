733862616c5b2bf9843179f0439414078dfde30b
Scott Cooper <scttcper@gmail.com>
2013-06-22 17:36:06 -0700

    try this

---
+++ localsettings.py
@@ -0,0 +1,14 @@
+def is_debug():
+    return True
+
+
+def get_token():
+    return "W28U7I8XM2NAMW6E"
+
+
+
+# JXZXEVNEBQAY99KN
+# 173.252.71.189 facebook vpn
+
+# W28U7I8XM2NAMW6E
+# 173.252.71.129 - work

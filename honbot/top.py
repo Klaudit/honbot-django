21127a3f8c7edd8843fba8f2c963531b12e90ed7
Scott Cooper <scttcper@gmail.com>
2013-06-23 22:44:41 -0700

    top chart kills

--- honbot/top.py
+++ honbot/top.py
@@ -1,10 +1,13 @@
 from django.template import Context, loader
 from django.http import HttpResponse
 from honbot.models import Matches, PlayerMatches
+from datetime import timedelta, datetime
 
 
 def main(request):
-    m = list(PlayerMatches.objects.filter(mode="rnk", d).)
-    t = loader.get_template('recent.html')
-    c = Context({'matches': m})
+    enddate = datetime.today()
+    startdate = enddate - timedelta(days=5)
+    kills = list(PlayerMatches.objects.filter(date__range=[startdate, enddate]).order_by('-kills')[:5])
+    t = loader.get_template('top.html')
+    c = Context({'kills': kills})
     return HttpResponse(t.render(c))

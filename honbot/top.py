334e735ba2daae450d37b81606fb6efbb34e13d6
Scott Cooper <scttcper@gmail.com>
2013-06-26 13:57:42 -0700

    Warning

--- honbot/top.py
+++ honbot/top.py
@@ -8,8 +8,8 @@ from django.db.models import Count
 def main(request):
     enddate = datetime.today()
     startdate = enddate - timedelta(days=5)
-    kills = list(PlayerMatches.objects.filter(date__range=[startdate, enddate]).order_by('-kills')[:5])
-    wards = list(PlayerMatches.objects.filter(date__range=[startdate, enddate]).order_by('-wards')[:5])
+    kills = PlayerMatches.objects.filter(date__range=[startdate, enddate]).order_by('-kills')[:5]
+    wards = PlayerMatches.objects.filter(date__range=[startdate, enddate]).order_by('-wards')[:5]
     mostplays = PlayerMatches.objects.filter(date__range=[startdate, enddate]).values('player_id', 'nickname').annotate(Count('player_id')).order_by('-player_id__count')[:5]
     t = loader.get_template('top.html')
     c = Context({'kills': kills, 'wards': wards, 'mostplays': mostplays})

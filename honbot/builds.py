0a458a81371854e61254f589aeb7ea4ecbc44d7b
Scott Cooper <scttcper@gmail.com>
2013-06-26 19:02:36 -0700

    That was my fault. Build fixed completely.

--- honbot/builds.py
+++ honbot/builds.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 import advanced
 from match import match
 from error import error
+import json
 
 
 def build(request, match_id):
@@ -10,13 +11,13 @@ def build(request, match_id):
     if data is not None:
         stats = match(match_id)
         builds = data.builds()
-        for i, p in enumerate(stats['players']):
-            if p is None:
-                stats['players'].pop(i)
+        statscomp = []
+        for i in range(0, 10):
+            if stats['players'][i] is not None:
+                statscomp.append(stats['players'][i])
         for i, build in enumerate(builds):
             if build is not None:
-                build.insert(0, stats['players'][0])
-                stats['players'].pop(0)
+                build.insert(0, statscomp[i])
         t = loader.get_template('build.html')
         c = Context({'builds': builds, 'match_id': match_id, 'stats': stats})
         return HttpResponse(t.render(c))

29a0b8f4740681db875499a1167ec3d9449eaaa1
Scott Cooper <scttcper@gmail.com>
2013-07-02 10:11:14 -0700

    fix querries

--- honbot/top.py
+++ honbot/top.py
@@ -9,13 +9,13 @@ import json
 def main(request):
     enddate = datetime.today()
     startdate = enddate - timedelta(days=5)
-    kills = PlayerMatches.objects.filter(date__range=[startdate, enddate]).order_by('-kills')[:5]
+    kills = PlayerMatches.objects.filter(date__range=[startdate, enddate], mode='rnk').order_by('-kills')[:5]
     for p in kills:
         p.items = json.loads(p.items)
-    wards = PlayerMatches.objects.filter(date__range=[startdate, enddate]).order_by('-wards').values('wards', 'nickname', 'win', 'match_id', 'hero')[:5]
-    assists = PlayerMatches.objects.filter(date__range=[startdate, enddate]).order_by('-assists').values('assists', 'nickname', 'win', 'match_id', 'hero')[:5]
-    deaths = PlayerMatches.objects.filter(date__range=[startdate, enddate]).order_by('-deaths').values('deaths', 'nickname', 'win', 'match_id', 'hero')[:5]
-    cs = PlayerMatches.objects.filter(date__range=[startdate, enddate]).order_by('-cs').values('cs', 'nickname', 'win', 'match_id', 'hero')[:5]
+    wards = PlayerMatches.objects.filter(date__range=[startdate, enddate], mode='rnk').order_by('-wards').values('wards', 'nickname', 'win', 'match_id', 'hero')[:5]
+    assists = PlayerMatches.objects.filter(date__range=[startdate, enddate], mode='rnk').order_by('-assists').values('assists', 'nickname', 'win', 'match_id', 'hero')[:5]
+    deaths = PlayerMatches.objects.filter(date__range=[startdate, enddate], mode='rnk').order_by('-deaths').values('deaths', 'nickname', 'win', 'match_id', 'hero')[:5]
+    cs = PlayerMatches.objects.filter(date__range=[startdate, enddate], mode='rnk').order_by('-cs').values('cs', 'nickname', 'win', 'match_id', 'hero')[:5]
     t = loader.get_template('top.html')
     c = Context({'kills': kills, 'wards': wards, 'deaths': deaths, 'assists': assists, 'cs': cs})
     return HttpResponse(t.render(c))

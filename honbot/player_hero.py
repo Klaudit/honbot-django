eaf4ffb91ad273fddb0eb99b74f528322441d5ef
Scott Cooper <scttcper@gmail.com>
2013-07-10 16:54:56 -0700

    player hero stats just 4 u

--- honbot/player_hero.py
+++ honbot/player_hero.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render_to_response
+import api_call
 
 
 def player_hero(request, name):
-    return render_to_response('player_hero.html', {})
+    return render_to_response('player_hero.html', {'player': name})
+
+
+def player_hero_stats(request, name, hero):
+    url = "/hero_statistics/ranked/nickname/" + name + "/heroid/" + hero
+    data = api_call.get_json(url)[0]
+    return render_to_response('player_hero_stats.html', {'data': data})

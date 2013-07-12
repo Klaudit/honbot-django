2cb40061ee9ff10a778d3146e9902bd5c876451e
Scott Cooper <scttcper@gmail.com>
2013-07-11 20:57:08 -0700

    more progress on hero stats

--- honbot/player_hero.py
+++ honbot/player_hero.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render_to_response
 import api_call
+from django.http import HttpResponse
 
 
 def player_hero(request, name):
@@ -8,5 +9,14 @@ def player_hero(request, name):
 
 def player_hero_stats(request, name, hero):
     url = "/hero_statistics/ranked/nickname/" + name + "/heroid/" + hero
-    data = api_call.get_json(url)[0]
-    return render_to_response('player_hero_stats.html', {'data': data})
+    data = api_call.get_json(url)
+    if data is not None:
+        hero = {}
+        hero['id'] = data[0]['hero_id']
+        hero['wins'] = data[0]['rnk_ph_wins']
+        hero['losses'] = data[0]['rnk_ph_losses']
+        hero['percent'] = int((float(data[0]['rnk_ph_wins']) / float(data[0]['rnk_ph_used'])) * 100)
+        hero['kills'] = round(float(data[0]['rnk_ph_herokills']) / float(data[0]['rnk_ph_used']), 1)
+        return render_to_response('player_hero_stats.html', {'hero': hero})
+    else:
+        return HttpResponse('Error! Stats not found')

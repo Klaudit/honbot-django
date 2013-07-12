81fba76fbd7d0e76002c1f2a890e4b6d93f3bf86
Scott Cooper <scttcper@gmail.com>
2013-07-11 23:28:10 -0700

    loading recent chached matches
    needs to save, fix urls

--- honbot/player_hero.py
+++ honbot/player_hero.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render_to_response
 import api_call
 from django.http import HttpResponse
+from honbot.models import PlayerMatches
+
+
+def divided(num1, num2):
+    try:
+        return float(num1) / float(num2)
+    except ZeroDivisionError:
+        return 0
 
 
 def player_hero(request, name):
@@ -15,8 +23,18 @@ def player_hero_stats(request, name, hero):
         hero['id'] = data[0]['hero_id']
         hero['wins'] = data[0]['rnk_ph_wins']
         hero['losses'] = data[0]['rnk_ph_losses']
-        hero['percent'] = int((float(data[0]['rnk_ph_wins']) / float(data[0]['rnk_ph_used'])) * 100)
-        hero['kills'] = round(float(data[0]['rnk_ph_herokills']) / float(data[0]['rnk_ph_used']), 1)
-        return render_to_response('player_hero_stats.html', {'hero': hero})
+        hero['percent'] = int(divided(data[0]['rnk_ph_wins'], data[0]['rnk_ph_used']) * 100)
+        hero['kills'] = round(divided(data[0]['rnk_ph_herokills'], data[0]['rnk_ph_used']), 1)
+        hero['deaths'] = round(divided(data[0]['rnk_ph_deaths'], data[0]['rnk_ph_used']), 1)
+        hero['kdr'] = round(divided(data[0]['rnk_ph_herokills'], data[0]['rnk_ph_deaths']), 1)
+        hero['consume'] = round(divided(data[0]['rnk_ph_consumables'], data[0]['rnk_ph_used']), 1)
+        hero['gpm'] = round(divided(data[0]['rnk_ph_gold'], divided(data[0]['rnk_ph_secs'], 60)), 1)
+        hero['apm'] = round(divided(data[0]['rnk_ph_actions'], divided(data[0]['rnk_ph_secs'], 60)), 1)
+        hero['exp'] = round(divided(data[0]['rnk_ph_exp'], divided(data[0]['rnk_ph_secs'], 60)), 1)
+        hero['cs'] = round(divided(data[0]['rnk_ph_teamcreepkills'], data[0]['rnk_ph_used']), 1)
+        hero['cd'] = round(divided(data[0]['rnk_ph_denies'], data[0]['rnk_ph_used']), 1)
+        hero['wards'] = round(divided(data[0]['rnk_ph_wards'], data[0]['rnk_ph_used']), 1)
+        recent = PlayerMatches.objects.filter(player_id=data[0]['account_id'], hero=data[0]['hero_id']).values()
+        return render_to_response('player_hero_stats.html', {'hero': hero, 'recent': recent})
     else:
         return HttpResponse('Error! Stats not found')

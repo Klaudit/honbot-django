8299b234c7438910fa3b85c69cc71fb35126b929
Scott Cooper <scttcper@gmail.com>
2013-08-16 12:38:26 -0700

    progress on charts

--- honbot/chart.py
+++ honbot/chart.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render_to_response
 from honbot.models import PlayerMatches, PlayerStats
+from error import error
+import pretty
 
 
 def view(request, name):
-    stats = PlayerStats.objects.filter(nickname=name).values()[0]
+    try:
+        stats = PlayerStats.objects.filter(nickname=name).values()[0]
+    except IndexError:
+        return error(request, "You may have spelled the players name incorrectly. Player stats missing.")
     matches = list(PlayerMatches.objects.filter(player_id=stats['player_id'], mode='rnk').order_by('match').values()[:50])
     count = len(matches)
     mmr = [0] * (count+1)
@@ -13,5 +18,6 @@ def view(request, name):
         mmr[i] = mmr[i+1] + (match['mmr_change'] * -1)
         i = i-1
     mmr = mmr[1:]
+    time = pretty.date(stats['updated'])
     match_list = [m['match_id'] for m in reversed(matches)]
-    return render_to_response('chart.html', {'mmr': mmr, 'match_list':match_list, 'stats':stats})
+    return render_to_response('chart.html', {'mmr':mmr, 'count':count, 'match_list':match_list, 'stats':stats, 'time':time})

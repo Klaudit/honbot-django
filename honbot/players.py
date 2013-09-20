09fc81386d8d18d24f429457372b23beeb8b18fa
Scott Cooper <scttcper@gmail.com>
2013-09-20 11:15:56 -0700

    TSR makes for some nice information

--- honbot/players.py
+++ honbot/players.py
@@ -1,16 +1,19 @@
 from django.shortcuts import render_to_response
 from honbot.models import PlayerStats
-from django.db.models import Count
 import numpy as np
 
 def view(requst):
-    players = PlayerStats.objects.values_list('mmr')
-    mmr, bins = [], []
+    players = PlayerStats.objects.values_list('mmr', 'TSR')
+    kills, mmr, bins = [], [], []
     for player in players:
         mmr.append(player[0])
+        print player[1]
+        kills.append(player[1])
     count = 1200
     for a in range(1,90):
         count = count + 10
         bins.append(count)
     mmr = np.histogram(mmr, bins=bins)
-    return render_to_response('players_stats.html', {'mmr':mmr[0], 'bins':mmr[1]})
+    kills = np.histogram(kills, bins=20, density=True)
+    print kills
+    return render_to_response('players_stats.html', {'mmr':mmr[0], 'mlable':mmr[1], 'kills':kills[0], 'klable':kills[1]})

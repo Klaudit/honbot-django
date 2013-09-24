b3cc325a80159d76809a8b3d820670e4c3b9c05a
Scott Cooper <scttcper@gmail.com>
2013-09-24 13:15:08 -0700

    add scraping script

---
+++ scripts/ladder_scrape.py
@@ -0,0 +1,17 @@
+# This script scrapes all usernames from the heroes of newerth ladder and creates a CSV
+import requests
+import json
+
+f = open('names.txt', 'w')
+for page in xrange(2245, 8180):
+    print page
+    url = "http://www.heroesofnewerth.com/ladder/view/ "+ str(page) + "?casual=false&jsonpls=1"
+    try:
+        raw = requests.get(url, timeout=12)
+    except requests.exceptions.Timeout:
+        pass
+    content = raw.json()
+    for player in content['rows']:
+        name = player[2][6:-7]
+        print name
+        f.write(name + ', ')

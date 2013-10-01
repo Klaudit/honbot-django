bdea40a5ffa0d9b5257f8fbbd56b31c4655595c8
Scott Cooper <scttcper@gmail.com>
2013-09-30 18:10:44 -0700

    custom command for remove matches

---
+++ honbot/management/commands/removenullmatches.py
@@ -0,0 +1,11 @@
+from honbot.models import Matches
+from django.core.management.base import BaseCommand, CommandError
+
+class Command(BaseCommand):
+	help = 'This will remove all matches that have a time length of 0 and probably no players'
+
+	def handle(self, *args, **options):
+		remove = Matches.objects.filter(realtime=0)
+		count = remove.count()
+		remove.delete()
+		self.stdout.write("Success! Deleted: " + str(count) + ' matches')

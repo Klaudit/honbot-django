01bc26e1bdd37bb6f562bc2c9ef976bdfc4c46cb
Scott Cooper <scttcper@gmail.com>
2013-10-09 14:35:40 -0700

    stats change

--- honbot/extra.py
+++ honbot/extra.py
@@ -5,8 +5,8 @@ from django.views.decorators.cache import cache_page
 
 
 def stats(request):
-    players = PlayerCount.objects.all()
-    matches = MatchCount.objects.all()
-    playermatches = PlayerMatchCount.objects.all()
+    players = PlayerCount.objects.all().order_by('-date')[:10][::-1]
+    matches = MatchCount.objects.all().order_by('-date')[:10][::-1]
+    playermatches = PlayerMatchCount.objects.all().order_by('-date')[:10][::-1]
     api = APICount.objects.all()
     return render_to_response('sitestats.html', {'players':players, 'matches':matches, 'playermatches':playermatches, 'api':api})

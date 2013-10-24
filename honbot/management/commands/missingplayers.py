9c2233cf0cf673e59e71dcffa93ee86c5bb6dd21
Scott Cooper <scttcper@gmail.com>
2013-10-24 15:44:27 -0700

    doing straight up sql right now

--- honbot/management/commands/missingplayers.py
+++ honbot/management/commands/missingplayers.py
@@ -1,6 +1,7 @@
 from django.core.management.base import BaseCommand, CommandError
 from honbot.api_call import get_json
 from honbot.models import PlayerStats
+from honbot.player import player_math, player_save, update_player_count
 
 
 class Command(BaseCommand):
@@ -8,6 +9,14 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         players = PlayerStats.objects.raw("""SELECT A.`player_id` FROM honbot_playerstats A LEFT JOIN honbot_playermatches B ON A.`player_id` = B.`player_id` WHERE B.`player_id` is NULL and A.`player_id` != 0""")
+        count = 0
         for player in players:
-            print player.player_id
-        self.stdout.write("success")
+            data = get_json('/player_statistics/ranked/accountid/' + str(player.player_id))
+            if data != None:
+                data = player_math(data, 'rnk')
+                player_save(data, 'rnk')
+                update_player_count()
+                count += 1
+            if count >= 100:
+                break
+        self.stdout.write("success on " + str(count))

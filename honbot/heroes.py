f39de6c42c713cf2b807e5d631886ae8bc7c089e
Scott Cooper <scttcper@gmail.com>
2013-11-03 18:27:07 -0800

    add parsing to effects

--- honbot/heroes.py
+++ honbot/heroes.py
@@ -14,6 +14,7 @@ def dmgreduction(armor):
         r = 0.94**abs(armor) - 1
     return '{0}%'.format(round(r,4) * 100)
 
+
 def iterable_attribute(name, supername, a, ability):
     try:
         a[name] = ability['attributes'][supername]
@@ -24,6 +25,50 @@ def iterable_attribute(name, supername, a, ability):
             a[name] = ' / '.join(a[name])
     except KeyError:
         a[name] = None
+
+def isDigit(c):
+    return c in ['0','1','2','3','4','5','6','7','8','9']
+
+
+def hon2html(msg):
+    hon_colors = ["00","1C","38","54","70","8C","A8","C4","E0","FF"]
+    msg = msg.replace('\\n','<br>')
+    parts = msg.split('^')
+    base = '<span>'
+    #msg = '<span style="color: white;">' + parts[0]
+    msg = base + parts[0]
+    for part in parts[1:]:
+        if len(part) < 1:
+            msg += '^'
+        elif part[0] == 'w' or part[0] == 'W':
+            msg += '</span><span style="color: white;">' + part[1:]
+        elif part[0] == 'r' or part[0] == 'R':
+            msg += '</span><span style="color: #FF4C4C;">' + part[1:]
+        elif part[0] == 'y' or part[0] == 'Y':
+            msg += '</span><span style="color: #FFFF19;">' + part[1:]
+        elif part[0] == 'g' or part[0] == 'G':
+            msg += '</span><span style="color: #008000;">' + part[1:]
+        elif part[0] == 'k' or part[0] == 'K':
+            msg += '</span><span style="color: #000000;">' + part[1:]
+        elif part[0] == 'c' or part[0] == 'C':
+            msg += '</span><span style="color: #00FFFF;">' + part[1:]
+        elif part[0] == 'b' or part[0] == 'B':
+            msg += '</span><span style="color: #4C4CFF;">' + part[1:]
+        elif part[0] == 't' or part[0] == 'T':
+            msg += '</span><span style="color: teal;">' + part[1:]
+        elif part[0] == 'o' or part[0] == 'O':
+            msg += '</span><span style="color: orange;">' + part[1:]
+        elif part[0] == 'm' or part[0] == 'M':
+            msg += '</span><span style="color: #FF00FF;">' + part[1:]
+        elif len(part) >= 3 and isDigit(part[0]) and part[1].isDigit() and isDigit(part[2]):
+            msg += '</span><span style="color: #%s%s%s;">' % (hon_colors[int(part[0])],hon_colors[int(part[1])],hon_colors[int(part[2])]) + part[3:]
+        elif part[0] == '*':
+            msg += '</span>' + base + part[1:]
+        elif part[0] in [';',':']:
+            msg += part[1:]
+        else:
+            msg += part
+    return msg + '</span>'
 
 
 def main(request):
@@ -82,6 +127,9 @@ def hero(request, name):
         iterable_attribute('range', 'RANGE', a, ability)
         iterable_attribute('casttime', 'RANGE', a, ability)
         iterable_attribute('manacost', 'MANACOST', a, ability)
+        iterable_attribute('cooldown', 'COOLDOWN', a, ability)
+        iterable_attribute('requiredlevel', 'REQUIREDLEVEL', a, ability)
+        a['effect'] = hon2html(ability['STRINGTABLE'][name + '_description_simple'])
         abilities[int(ability['ab_slot'])] = a
     return render_to_response('hero.html',
         {'hero': h, 'use': use, 'popularity': popularity, 'minmax': minmax, 'lvl1': lvl1, 'lvl25': lvl25, 'abilities': abilities})

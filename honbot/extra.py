518883b9615ddf11803a28d4b0c27ec016abcb22
Scott Cooper <scttcper@gmail.com>
2013-11-04 00:49:42 -0800

    add cron jobs to site stats

--- honbot/extra.py
+++ honbot/extra.py
@@ -1,12 +1,14 @@
 # This file is intended for extra pages like site stats and possibly an about page
 from django.shortcuts import render_to_response
 from honbot.models import PlayerMatchCount, PlayerCount, MatchCount, APICount
+from django_cron.models import CronJobLog
 from django.views.decorators.cache import cache_page
 
-
+@cache_page(1000)
 def stats(request):
     players = PlayerCount.objects.all().order_by('-date')[:10][::-1]
     matches = MatchCount.objects.all().order_by('-date')[:10][::-1]
     playermatches = PlayerMatchCount.objects.all().order_by('-date')[:10][::-1]
+    cronjobs = CronJobLog.objects.all()[:20]
     api = APICount.objects.all()
-    return render_to_response('sitestats.html', {'players':players, 'matches':matches, 'playermatches':playermatches, 'api':api})
+    return render_to_response('sitestats.html', {'players':players, 'matches':matches, 'playermatches':playermatches, 'api':api, 'cronjobs': cronjobs})

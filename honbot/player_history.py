f11b15c769e43ad674f50b154a6bbdcd33c83914
Scott Cooper <scttcper@gmail.com>
2013-11-04 14:16:25 -0800

    convert to spaces

--- honbot/player_history.py
+++ honbot/player_history.py
@@ -21,70 +21,70 @@ def history_public(request, account_id, page):
     return history(request, account_id, "acc", url, page)
 
 def history(request, account_id, mode, url, page):
-	"""
-	this is the main function of player history
-	"""
-	phistory = PlayerHistory.objects.filter(player_id=account_id, mode=mode)
-	count = int(page) * return_size
-	# if history exists check the age
-	if phistory.exists():
-		# find age
-		existing = phistory.values()[0]
-		old = existing['id']
-		tdelta = datetime.now() - datetime.strptime(str(existing['updated']), "%Y-%m-%d %H:%M:%S")
-		if tdelta.seconds + (tdelta.days * 86400) < 1080:
-			data = loads(existing['history'])
-		else:
-			data = update_history(url, account_id, mode, True)
-			delete = PlayerHistory.objects.get(id=old).delete()
-	else:
-		data = update_history(url, account_id, mode, False)
-	verify_matches(data[(count-return_size):count], mode)
-	matches = PlayerMatches.objects.filter(match_id__in=data[(count-return_size):count], player_id=account_id).order_by('-date').values()
-	if len(matches) != 0:
-		return render_to_response('player_history.html', {'matches': matches})
-	else:
-		return HttpResponse('stop')
+    """
+    this is the main function of player history
+    """
+    phistory = PlayerHistory.objects.filter(player_id=account_id, mode=mode)
+    count = int(page) * return_size
+    # if history exists check the age
+    if phistory.exists():
+        # find age
+        existing = phistory.values()[0]
+        old = existing['id']
+        tdelta = datetime.now() - datetime.strptime(str(existing['updated']), "%Y-%m-%d %H:%M:%S")
+        if tdelta.seconds + (tdelta.days * 86400) < 1080:
+            data = loads(existing['history'])
+        else:
+            data = update_history(url, account_id, mode, True)
+            delete = PlayerHistory.objects.get(id=old).delete()
+    else:
+        data = update_history(url, account_id, mode, False)
+    verify_matches(data[(count-return_size):count], mode)
+    matches = PlayerMatches.objects.filter(match_id__in=data[(count-return_size):count], player_id=account_id).order_by('-date').values()
+    if len(matches) != 0:
+        return render_to_response('player_history.html', {'matches': matches})
+    else:
+        return HttpResponse('stop')
 
 def update_history(url, account_id, mode, exists):
-	"""
-	Updates a player's history and saves it to db. [] is saved if no result
-	"""
-	raw = get_json(url)
-	# if no recent matches just save an empty array
-	try:
-		raw = raw[0]['history']
-	except:
-		# try to fallback if api down
-		if exists:
-			data = loads(PlayerHistory.objects.filter(player_id=account_id, mode=mode).values()[0]['history'])
-			PlayerHistory(player_id=account_id, history=dumps(data), mode=mode).save()
-			return data
-		else:
-			PlayerHistory(player_id=account_id, history=dumps([]), mode=mode).save()
-			return []
-	data = []
-	for match in raw.split(','):
-		if len(match) > 20: # this fixes an error on broken player histories
-			data.append(int(match.split('|')[0]))
-	PlayerHistory(player_id=account_id, history=dumps(data[::-1]), mode=mode).save()
-	return data[::-1]
+    """
+    Updates a player's history and saves it to db. [] is saved if no result
+    """
+    raw = get_json(url)
+    # if no recent matches just save an empty array
+    try:
+        raw = raw[0]['history']
+    except:
+        # try to fallback if api down
+        if exists:
+            data = loads(PlayerHistory.objects.filter(player_id=account_id, mode=mode).values()[0]['history'])
+            PlayerHistory(player_id=account_id, history=dumps(data), mode=mode).save()
+            return data
+        else:
+            PlayerHistory(player_id=account_id, history=dumps([]), mode=mode).save()
+            return []
+    data = []
+    for match in raw.split(','):
+        if len(match) > 20: # this fixes an error on broken player histories
+            data.append(int(match.split('|')[0]))
+    PlayerHistory(player_id=account_id, history=dumps(data[::-1]), mode=mode).save()
+    return data[::-1]
 
 def verify_matches(data, mode):
-	"""
-	this checks for matches to exist in the database. If they do not exist they are then downloaded.
-	"""
-	findexisting = Matches.objects.filter(match_id__in=data).values('match_id')
-	existing = set([int(match['match_id']) for match in findexisting])
-	missing = [x for x in data if x not in existing]
-	# if any are missing FIND THEM
-	if len(missing) != 0:
-		strmatches = ''
-		for match in missing:
-			strmatches += str(match)
-			if match != missing[-1]:
-				strmatches += '+'
-		url = '/multi_match/all/matchids/' + strmatches
-		raw = get_json(url)
-		if raw != None:
-			multimatch(raw, missing, mode)
+    """
+    this checks for matches to exist in the database. If they do not exist they are then downloaded.
+    """
+    findexisting = Matches.objects.filter(match_id__in=data).values('match_id')
+    existing = set([int(match['match_id']) for match in findexisting])
+    missing = [x for x in data if x not in existing]
+    # if any are missing FIND THEM
+    if len(missing) != 0:
+        strmatches = ''
+        for match in missing:
+            strmatches += str(match)
+            if match != missing[-1]:
+                strmatches += '+'
+        url = '/multi_match/all/matchids/' + strmatches
+        raw = get_json(url)
+        if raw != None:
+            multimatch(raw, missing, mode)

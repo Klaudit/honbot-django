862d2fad08a70209d7b698d99dd4560191e24e26
Scott Cooper <scttcper@gmail.com>
2013-11-03 18:11:37 -0800

    hero progress

--- honbot/heroes.py
+++ honbot/heroes.py
@@ -14,6 +14,17 @@ def dmgreduction(armor):
         r = 0.94**abs(armor) - 1
     return '{0}%'.format(round(r,4) * 100)
 
+def iterable_attribute(name, supername, a, ability):
+    try:
+        a[name] = ability['attributes'][supername]
+        if ',' in a[name]:
+            a[name] = a[name].split(',')
+            for c in a[name]:
+                c = float(c) / 1000
+            a[name] = ' / '.join(a[name])
+    except KeyError:
+        a[name] = None
+
 
 def main(request):
     heroes = Heroes.objects.all().values()
@@ -53,11 +64,24 @@ def hero(request, name):
     lvl25['hpregeneration'] = h['healthregen'] + (0.03 * (h['strength'] + 20 + floor(h['strengthperlevel'] * 24)))
     lvl1['mpregeneration'] = h['manaregen'] + (0.04 * h['intelligence'])
     lvl25['mpregeneration'] = h['manaregen'] + (0.04 * (h['intelligence'] + 20 + floor(h['intelligenceperlevel'] * 24)))
-    abilities = []
+    abilities = [0] * 4
     for ability in [loads(h['ability1'])[1], loads(h['ability2'])[1], loads(h['ability3'])[1], loads(h['ability4'])[1]]:
         a = {}
         name = ability['cli_ab_name']
         a['name'] = ability['STRINGTABLE'][name + '_name']
-        abilities.append(a)
+        a['description'] = ability['STRINGTABLE'][name + '_description']
+        a['actiontype'] = ability['attributes']['ACTIONTYPE'].replace('_', ' ').title()
+        try:
+            a['targetscheme'] = ability['attributes']['TARGETSCHEME'].replace('_', ' ').title()
+        except KeyError:
+            a['targetscheme'] = None
+        try:
+            a['casteffecttype'] = ability['attributes']['CASTEFFECTTYPE'].replace('_', ' ').title()
+        except KeyError:
+            a['casteffecttype'] = None
+        iterable_attribute('range', 'RANGE', a, ability)
+        iterable_attribute('casttime', 'RANGE', a, ability)
+        iterable_attribute('manacost', 'MANACOST', a, ability)
+        abilities[int(ability['ab_slot'])] = a
     return render_to_response('hero.html',
         {'hero': h, 'use': use, 'popularity': popularity, 'minmax': minmax, 'lvl1': lvl1, 'lvl25': lvl25, 'abilities': abilities})

9d0d90c379c60840dacaa69a2fc568d4013c8092
Scott Cooper <scttcper@gmail.com>
2013-11-11 21:33:05 -0800

    fix hero rank

--- honbot/heroes.py
+++ honbot/heroes.py
@@ -85,7 +85,8 @@ def main(request):
 def hero(request, name):
     h = HeroData.objects.filter(cli_name=name).values()[0]
     use = HeroUse.objects.filter(hero_id=h['hero_id'])[:10]
-    popularity = HeroUse.objects.filter(hero_id=h['hero_id'])[:1][0]
+    today = date.today().strftime("%Y-%m-%d")
+    popularity = HeroUse.objects.get(hero_id=h['hero_id'], date=date.today().strftime("%Y-%m-%d"))
     minmax = HeroData.objects.aggregate(Max('movespeed'),Min('movespeed'),
         Max('turnrate'),Min('turnrate'),
         Max('strengthperlevel'),Min('strengthperlevel'),

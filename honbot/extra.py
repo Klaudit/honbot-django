99c9625cb9cc364ed67afc8db7f0f5840780d30e
Scott Cooper <scttcper@gmail.com>
2014-01-12 17:43:43 -0800

    setup stats page to remove players in matches

--- honbot/extra.py
+++ honbot/extra.py
@@ -3,12 +3,14 @@ from django.shortcuts import render_to_response
 from .models import PlayerMatchCount, PlayerCount, MatchCount, APICount
 from django_cron.models import CronJobLog
 from django.views.decorators.cache import cache_page
+from django.db.models import Avg
 
 
+@cache_page(1000)
 def stats(request):
-    players = PlayerCount.objects.all().order_by('-date')[:10][::-1]
-    matches = MatchCount.objects.all().order_by('-date')[:10][::-1]
-    playermatches = PlayerMatchCount.objects.all().order_by('-date')[:10][::-1]
+    players = PlayerCount.objects.all().order_by('-date')[:20][::-1]
+    matches = MatchCount.objects.all().order_by('-date')[:20][::-1]
     cronjobs = CronJobLog.objects.all().order_by('-start_time')[:20]
-    api = APICount.objects.all()
-    return render_to_response('sitestats.html', {'players':players, 'matches':matches, 'playermatches':playermatches, 'api':api, 'cronjobs': cronjobs})
+    api = APICount.objects.all().order_by('-date')[:20][::-1]
+    apiavg = int(APICount.objects.all().aggregate(Avg('count'))['count__avg'])
+    return render_to_response('sitestats.html', {'players':players, 'matches':matches, 'api':api, 'cronjobs': cronjobs, 'apiavg': apiavg})

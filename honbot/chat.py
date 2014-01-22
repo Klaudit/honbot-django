e815a02eae0f4d1a721611f5bd0de86eb88d6129
Scott Cooper <scttcper@gmail.com>
2014-01-22 11:38:23 -0800

    after lots of testing I'm feeling much better about chat logs

--- honbot/chat.py
+++ honbot/chat.py
@@ -7,19 +7,20 @@ from logparse import download, parse
 
 def chat_view(request, match_id):
     match = Matches.objects.filter(match_id=match_id).values()
-    if match.exists():
-        chat = Chat.objects.filter(match_id=match_id).values('json')
-        if chat.exists():
-            chat = chat[0]
-            match = match[0]
+    if len(match) > 0:
+        chat = Chat.objects.filter(match_id=match_id).values()
+        if len(chat) > 0:
+            match, chat = match[0], chat[0]
             match['date'] = datetime.strptime(str(match['date']), '%Y-%m-%d %H:%M:%S') - timedelta(hours=1)
             chat['json'] = loads(chat['json'])
             PMObj = pmoselect(match['mode'])
-            players = PMObj.objects.filter(match=match_id).order_by('position')
+            players = PMObj.objects.filter(match=match_id).order_by('position')[:1]
             return render_to_response('chat.html', {'chat': chat['json'], 'match':match, 'players':players})
         else:
+            PMObj = pmoselect(match[0]['mode'])
+            players = PMObj.objects.filter(match_id=match_id).order_by('position').values()
             if download(match_id, match[0]['replay_url']):
-                if parse(match_id):
+                if parse(match_id, players):
                     return chat_view(request, match_id)
                 else:
                     return error(request, "The chat logs had trouble somewhere.<br> We all have bad days sometimes. Sorry")
--- honbot/logparse.py
+++ honbot/logparse.py
@@ -7,10 +7,24 @@ from requests import get
 from time import strftime, gmtime
 from zipfile import ZipFile
 
-
+# Directory where match logs and zips will be saved, unzipped, and read from
 directory = str(path.join(path.abspath(path.dirname(path.dirname(__file__))), 'match')) + '/'
 
 
+# calls all appropriate functions in correct order
+# creates the honlog class object
+def parse(match_id, players):
+    log = honlog(match_id, players)
+    log.open_file()
+    log.run()
+    if log.save():
+        return True
+    else:
+        return False
+    log.delete()
+    return True
+
+# downloads match, returns false if unable to download
 def download(match_id, url):
     url = url[:-9] + 'zip'
     # download file
@@ -28,27 +42,20 @@ def download(match_id, url):
     remove(directory + str(match_id) + '.zip')
     return True
 
-
-def parse(match_id):
-    log = honlog(match_id)
-    log.setup()
-    log.open_file()
-    log.run()
-    # log.out()
-    log.save()
-    log.delete()
-    return True
-
-
+# the parsing class
 class honlog:
-    def __init__(self, match_id):
+    def __init__(self, match_id, players):
         self.match_id = match_id
-        self.real_order, self.order_convert, self.teams = {}, {}, {}
+        self.matchpos, self.playerinfo, self.posfind, self.spectators = {}, {}, {}, {}
         self.logfile = file
         self.date, self.time = '', ''
-        self.names, self.psr, self.heroes, self.win, self.msg = [0] * 10, [0] * 10, [0] * 10, [0] * 10, []
-        self.builds = [[] for i in range(10)]
+        self.msg = []
+        for player in players:
+            self.matchpos[str(player['player_id'])] = player['position']
+            self.playerinfo[str(player['position'])] = player
 
+    ## non parsing functions ##
+    # loops over each line in log file
     def run(self):
         for line in self.logfile:
             word = line.split()[0]
@@ -58,26 +65,37 @@ class honlog:
             except AttributeError:
                 pass
 
+    # deletes logfile from self.match_id
     def delete(self):
         remove(directory + 'm' + str(self.match_id) + '.log')
 
-    def setup(self):
-        """
-        This gets the proper order of players from the db
-        creates self.real_order[player_id]=real_position
-        """
-        positions = PlayerMatches.objects.filter(match_id=self.match_id).order_by('position').values('position', 'player_id', 'hero', 'team', 'win')
-        for p in positions:
-            pid = str(p['player_id'])
-            print str(p['position']) + " " + pid
-            self.real_order[pid] = p['position']
-            self.heroes[p['position']] = p['hero']
-            self.teams[p['position']] = p['team']
-            self.win[p['position']] = p['win']
-
+    # opens self.match_id with correct encoding
     def open_file(self):
         self.logfile = op(directory + 'm' + self.match_id + '.log', encoding='utf-16-le', mode='rb').readlines()
         self.INFO_DATE(self.logfile.pop(0))
+
+    # returns data from the player model copied earlier into an array
+    def getinfo(self, chat_pos, req):
+        return self.playerinfo[str(self.posfind[str(chat_pos)])][req]
+
+    # converts time from some sort of milisecond into nice formatting
+    # uses hours only if longer than an hour
+    def set_time(self, time):
+        if int(time) < 3599999:
+            time = strftime('%M:%S', gmtime(int(time) // 1000))
+        else:
+            time = strftime('%H:%M:%S', gmtime(int(time) // 1000))
+        return time
+
+    # saves chat mesages to the model
+    def save(self):
+        if len(self.msg) > 0:
+            Chat(match_id=self.match_id, json=dumps(self.msg)).save()
+            return True
+        else:
+            return False
+
+    ## parsing functions ##
 
     def INFO_DATE(self, line):
         """
@@ -93,19 +111,23 @@ class honlog:
         PLAYER_CONNECT player:0 name:"NAMENAMENAME" id:3252583 psr:1522.0000
         spectators too
         PLAYER_CONNECT time:1617600 player:13 name:"[bMcE]Smexystyle`" id:7399320 psr:-1.0000
+        PLAYER_CONNECT player:10 name:"[ATMS]Gingersnapz" id:6022781 psr:-1.0000
         """
         l = line.split()
-        psr = int(float(l[-1].split(':')[1]))
-        name = l[2].split(':')[1][1:-1]  # get name
-        name = name.split(']')[-1]  # remove clan tag
         pid = str(l[-2].split(':')[1])
-        if pid in self.real_order:
-            position = int(l[1].split(':')[1])
-            self.names[self.real_order[pid]] = name
-            self.psr[self.real_order[pid]] = psr
-            self.order_convert[position] = self.real_order[pid]
+        if self.matchpos.has_key(str(pid)):
+            player = int(l[1].split(':')[1])
+            self.posfind[str(player)] = self.matchpos[pid]
         else:
-            self.spectators[position] = name
+            if len(l) == 5:
+                name = l[2].split(':')[1][1:-1]  # get name
+                name = name.split(']')[-1]  # remove clan tag
+                player = int(l[1].split(':')[1])
+            else:
+                name = l[3].split(':')[1][1:-1]  # get name
+                name = name.split(']')[-1]  # remove clan tag
+                player = int(l[2].split(':')[1])
+            self.spectators[player] = name
 
     def HERO_DEATH(self, line):
         """
@@ -117,65 +139,20 @@ class honlog:
         kill = {}
         kill['kill'] = True
         kill['time'] = self.set_time(l[1].split(':')[1])
-        try:
-            kill['killer'] = self.position(int(sub("\D", "", l[-1])))
-            kill['herokiller'] = self.heroes[kill['killer']]
-            kill['killername'] = self.names[kill['killer']]
-        except:
+        if "owner:" not in line:
             kill['killer'] = 42
-            kill['herokiller'] = 42
-            kill['killername'] = 42
-        kill['killed'] = self.position(int(sub("\D", "", l[5])))
-        kill['herokilled'] = self.heroes[kill['killed']]
-        kill['killedname'] = self.names[kill['killed']]
+            kill['herokiller'] = None
+            kill['killername'] = "Neutrals"
+        else:
+            killerpos = int(sub("\D", "", l[-1]))
+            kill['killer'] = self.getinfo(killerpos, 'position')
+            kill['herokiller'] = self.getinfo(killerpos, 'hero')
+            kill['killername'] = self.getinfo(killerpos, 'nickname')
+        killedpos = int(sub("\D", "", l[5]))
+        kill['killed'] = self.getinfo(killedpos, 'position')
+        kill['herokilled'] = self.getinfo(killedpos, 'hero')
+        kill['killedname'] = self.getinfo(killedpos, 'nickname')
         self.msg.append(kill)
-
-
-    def save(self):
-        c = Chat(match_id=self.match_id, json=dumps(self.msg))
-        c.save()
-        for index, build in enumerate(self.builds):
-            if len(build) != 0 and self.heroes[index] != 0:
-                difference = 25 - len(build)
-                for d in range(difference):
-                    build.append(0)
-                if len(build) > 25:
-                    build = build[:25]
-                Builds(match_id=self.match_id, json=dumps(build[:25-difference]),
-                           hero=self.heroes[index], nickname=self.names[index],
-                           mmr=self.psr[index], win=self.win[index], position=index,
-                           lvl1=build[0], lvl2=build[1], lvl3=build[2], lvl4=build[3],
-                           lvl5=build[4], lvl6=build[5], lvl7=build[6], lvl8=build[7],
-                           lvl9=build[8], lvl10=build[9], lvl11=build[10], lvl12=build[11],
-                           lvl13=build[12], lvl14=build[13], lvl15=build[14], lvl16=build[15],
-                           lvl17=build[16], lvl18=build[17], lvl19=build[18], lvl20=build[19],
-                           lvl21=build[20], lvl22=build[21], lvl23=build[22], lvl24=build[23], lvl25=build[24]).save()
-
-    def set_time(self, time):
-        if int(time) < 3599999:
-            time = strftime('%M:%S', gmtime(int(time) // 1000))
-        else:
-            time = strftime('%H:%M:%S', gmtime(int(time) // 1000))
-        return time
-
-    def position(self, position):
-        if position in self.order_convert:
-            return self.order_convert[position]
-        else:
-            return 9
-
-    def ABILITY_UPGRADE(self, line):
-        """
-        ABILITY_UPGRADE time:0 x:1662 y:995 z:101 player:1 team:1 name:"Ability_Empath1" level:1 slot:0
-        """
-        if "Ability_Taunt" not in line:
-            l = line.split()
-            ability = l[7].split('"')[1]
-            if ability != 'Ability_AttributeBoost':
-                ability = int(sub("\D", "", ability)[-1:])
-            else:
-                ability = 5
-            self.builds[self.position(int(l[5].split(':')[1]))].append(ability)
 
     def PLAYER_CHAT(self, line):
         """
@@ -187,32 +164,28 @@ class honlog:
         chat['msg'] = ''
         l = line.split()
         if line[12] == 'p':
-            chat['position'] = self.position(int(l[1].split(':')[1]))
-            chat['name'] = self.names[chat['position']]
+            pos = l[1].split(':')[1]
+            chat['position'] = self.getinfo(pos, 'position')
+            chat['name'] = self.getinfo(pos, 'nickname')
             chat['target'] = l[2].split(':')[1][1:-1]
-            chat['hero'] = self.heroes[chat['position']]
+            chat['hero'] = self.getinfo(pos, 'hero')
             chat['time'] = "Lobby"
             chat['msg'] = line.split('msg:')[1]
             if chat['target'] == 'all':
                 chat['target'] = 3
             else:
-                chat['target'] = self.teams[self.position(int(l[1].split(':')[1]))]
+                chat['target'] = self.getinfo(pos, 'team')
         else:
-            chat['position'] = self.position(int(l[2].split(':')[1]))
+            pos = l[2].split(':')[1]
+            chat['position'] = self.getinfo(pos, 'position')
             chat['time'] = self.set_time(l[1].split(':')[1])
-            chat['name'] = self.names[chat['position']]
+            chat['name'] = self.getinfo(pos, 'nickname')
             chat['target'] = l[3].split(':')[1][1:-1]
             chat['msg'] = line.split('msg:')[1]
-            chat['hero'] = self.heroes[chat['position']]
+            chat['hero'] = self.getinfo(pos, 'hero')
             if chat['target'] == 'all':
                 chat['target'] = 3
             else:
-                chat['target'] = self.teams[self.position(int(l[2].split(':')[1]))]
+                chat['target'] = self.getinfo(pos, 'team')
         chat['msg'] = chat['msg'][1:-3]
         self.msg.append(chat)
-
-    def out(self):
-        print self.real_order
-        print self.names
-        print self.order_convert
-        print self.builds

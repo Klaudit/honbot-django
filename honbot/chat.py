b4a32e6dd0136b120cb8c23abf13653bf81f16aa
Scott Cooper <scttcper@gmail.com>
2014-01-27 07:26:06 -0800

    spacing

--- honbot/chat.py
+++ honbot/chat.py
@@ -5,6 +5,7 @@ from .models import Chat, Matches, PlayerMatches, PlayerMatchesPublic, PlayerMat
 from json import loads
 from logparse import download, parse
 
+
 def chat_view(request, match_id):
     match = Matches.objects.filter(match_id=match_id).values()
     if len(match) > 0:
@@ -15,7 +16,7 @@ def chat_view(request, match_id):
             chat['json'] = loads(chat['json'])
             PMObj = pmoselect(match['mode'])
             players = PMObj.objects.filter(match=match_id).order_by('position')[:1]
-            return render_to_response('chat.html', {'chat': chat['json'], 'match':match, 'players':players})
+            return render_to_response('chat.html', {'chat': chat['json'], 'match': match, 'players': players})
         else:
             PMObj = pmoselect(match[0]['mode'])
             players = PMObj.objects.filter(match_id=match_id).order_by('position').values()

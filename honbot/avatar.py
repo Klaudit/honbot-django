b11df4c54659ca24c7f997d6685e7ae5609a2b8d
Scott Cooper <scttcper@gmail.com>
2014-03-30 21:54:39 -0700

    fix broken avatars

--- honbot/avatar.py
+++ honbot/avatar.py
@@ -10,7 +10,7 @@ def avatar(request, number, width):
     p = PlayerIcon.objects.filter(player_id=number)
     if p.exists():
         tdelta = datetime.now() - datetime.strptime(str(p.values()[0]['updated']), "%Y-%m-%d %H:%M:%S")
-        if tdelta.days < 20:
+        if tdelta.days < 25:
             return HttpResponse('<img style="width:' + str(width) + 'px;" src="' + p.values('avatar')[0]['avatar'] + '">')
     opener = build_opener()
     curl = settings.PHP
@@ -22,6 +22,8 @@ def avatar(request, number, width):
     except:
         PlayerIcon(player_id=number, avatar="/static/img/default_avatar.png").save()
         return HttpResponse('<img style="width:' + str(width) + 'px;" src="/static/img/default_avatar.png">')
+    if "account_icons" in img:
+        img = 'http://forums.heroesofnewerth.com/' + img
     if img and img != "http://forums.heroesofnewerth.com/" and img != "http://forums.heroesofnewerth.com//ui/fe2/store/icons/community.tga" and img != 'images/account_icons/default.png':
         PlayerIcon(player_id=number, avatar=str(img)).save()
         return HttpResponse('<img style="width:' + str(width) + 'px;" src="' + img + '">')

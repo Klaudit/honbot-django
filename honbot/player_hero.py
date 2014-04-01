eab442193129d982d3bee2a5c8c0bfa75968ccea
Scott Cooper <scttcper@gmail.com>
2014-04-01 14:58:40 -0700

    spacing

--- honbot/player_hero.py
+++ honbot/player_hero.py
@@ -12,6 +12,7 @@ def divided(num1, num2):
     except ZeroDivisionError:
         return 0
 
+
 def ranked_view(request, name):
     try:
         stats = PlayerStats.objects.filter(nickname=name).values()[0]
@@ -20,12 +21,14 @@ def ranked_view(request, name):
         return redirect('/player/%s/' % name)
     return render_to_response('player_hero.html', {'player': name, 'stats': stats, 'mode': "rnk", 'view': "player_hero", 'heroes': heroes})
 
+
 def casual_view(request, name):
     try:
         stats = PlayerStatsCasual.objects.filter(nickname=name).values()[0]
     except:
         return redirect('/c/player/%s/' % name)
     return render_to_response('player_hero.html', {'player': name, 'stats': stats, 'mode': "cs", 'view': "player_hero"})
+
 
 def public_view(request, name):
     try:
@@ -34,14 +37,18 @@ def public_view(request, name):
         return redirect('/p/player/%s/' % name)
     return render_to_response('player_hero.html', {'player': name, 'stats': stats, 'mode': "acc", 'view': "player_hero"})
 
+
 def ph_ranked(request, name, hero):
     return player_hero_stats(request, name, hero, "rnk", "ranked")
+
 
 def ph_casual(request, name, hero):
     return player_hero_stats(request, name, hero, "cs", "casual")
 
+
 def ph_public(request, name, hero):
     return player_hero_stats(request, name, hero, "acc", "public")
+
 
 def player_hero_stats(request, name, hero, mode, modename):
     search = PlayerHeroStats.objects.filter(nickname=name, hero_id=hero, mode=mode)
@@ -65,19 +72,19 @@ def player_hero_stats(request, name, hero, mode, modename):
             mode = mode + '_'
         hero = {}
         hero['id'] = data[0]['hero_id']
-        hero['wins'] = data[0][mode+ 'ph_wins']
-        hero['losses'] = data[0][mode+ 'ph_losses']
-        hero['percent'] = int(divided(data[0][mode+ 'ph_wins'], data[0][mode+ 'ph_used']) * 100)
-        hero['kills'] = round(divided(data[0][mode+ 'ph_herokills'], data[0][mode+ 'ph_used']), 1)
-        hero['deaths'] = round(divided(data[0][mode+ 'ph_deaths'], data[0][mode+ 'ph_used']), 1)
-        hero['kdr'] = round(divided(data[0][mode+ 'ph_herokills'], data[0][mode+ 'ph_deaths']), 1)
-        hero['consume'] = round(divided(data[0][mode+ 'ph_consumables'], data[0][mode+ 'ph_used']), 1)
-        hero['gpm'] = round(divided(data[0][mode+ 'ph_gold'], divided(data[0][mode+ 'ph_secs'], 60)), 1)
-        hero['apm'] = round(divided(data[0][mode+ 'ph_actions'], divided(data[0][mode+ 'ph_secs'], 60)), 1)
-        hero['exp'] = round(divided(data[0][mode+ 'ph_exp'], divided(data[0][mode+ 'ph_secs'], 60)), 1)
-        hero['cs'] = round(divided(data[0][mode+ 'ph_teamcreepkills'], data[0][mode+ 'ph_used']), 1)
-        hero['cd'] = round(divided(data[0][mode+ 'ph_denies'], data[0][mode+ 'ph_used']), 1)
-        hero['wards'] = round(divided(data[0][mode+ 'ph_wards'], data[0][mode+ 'ph_used']), 1)
+        hero['wins'] = data[0][mode + 'ph_wins']
+        hero['losses'] = data[0][mode + 'ph_losses']
+        hero['percent'] = int(divided(data[0][mode + 'ph_wins'], data[0][mode + 'ph_used']) * 100)
+        hero['kills'] = round(divided(data[0][mode + 'ph_herokills'], data[0][mode + 'ph_used']), 1)
+        hero['deaths'] = round(divided(data[0][mode + 'ph_deaths'], data[0][mode + 'ph_used']), 1)
+        hero['kdr'] = round(divided(data[0][mode + 'ph_herokills'], data[0][mode + 'ph_deaths']), 1)
+        hero['consume'] = round(divided(data[0][mode + 'ph_consumables'], data[0][mode + 'ph_used']), 1)
+        hero['gpm'] = round(divided(data[0][mode + 'ph_gold'], divided(data[0][mode + 'ph_secs'], 60)), 1)
+        hero['apm'] = round(divided(data[0][mode + 'ph_actions'], divided(data[0][mode + 'ph_secs'], 60)), 1)
+        hero['exp'] = round(divided(data[0][mode + 'ph_exp'], divided(data[0][mode + 'ph_secs'], 60)), 1)
+        hero['cs'] = round(divided(data[0][mode + 'ph_teamcreepkills'], data[0][mode + 'ph_used']), 1)
+        hero['cd'] = round(divided(data[0][mode + 'ph_denies'], data[0][mode + 'ph_used']), 1)
+        hero['wards'] = round(divided(data[0][mode + 'ph_wards'], data[0][mode + 'ph_used']), 1)
         return render_to_response('player_hero_stats.html', {'hero': hero})
     else:
         return HttpResponse('Error! Returned no stats with this hero.')

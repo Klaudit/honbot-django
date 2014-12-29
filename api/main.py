e8845a54115b0f7e3c59429246331fbde6316f76
cooper <scttcper@gmail.com>
2014-12-29 10:48:32 -0800

    new models and migrations

--- api/main.py
+++ api/main.py
@@ -8,10 +8,10 @@ from banner import bannerapp
 
 def register_blueprints(app):
     app.register_blueprint(players)
-    # app.register_blueprint(matches)
+    app.register_blueprint(matches)
     app.register_blueprint(history)
-    # app.register_blueprint(stats)
-    # app.register_blueprint(bannerapp)
+    app.register_blueprint(stats)
+    app.register_blueprint(bannerapp)
     return None
 
 
--- api/matches.py
+++ api/matches.py
@@ -1,6 +1,8 @@
 from api import get_json
 from utils import divmin, div
 from app import db
+from models import Match, PlayerMatch
+from serialize import MatchSchema
 
 from pytz import utc
 from flask import jsonify, Blueprint, abort
@@ -16,15 +18,17 @@ def match(mid):
     Returns a single match from ID
     Players are returned in an array and their items are a json array dumped into a string
     """
-    m = db.matches.find_one({'_id': mid})
+    m = Match.query.filter_by(id=mid).first()
     if m is None:
         raw = get_json('/match/all/matchid/' + str(mid))
         if raw:
             m = single_match(raw, mid)
-            db.matches.insert(m)
+            db.session.add(m)
+            db.session.commit()
         else:
             abort(404)
-    return jsonify(m)
+    result = MatchSchema().dump(m)
+    return jsonify(result[0])
 
 
 def multimatch(matches):
@@ -45,19 +49,22 @@ def multimatch(matches):
 
 
 def single_match(raw, mid):
-    m = {'_id': int(mid)}
+    m = Match(id=mid)
     if raw[0][0]['officl'] == "1" and raw[0][0]['cas'] == "1":
-        m['mode'] = 'cs'
+        # m.mode = 'cs'
+        m.mode = 2
     elif raw[0][0]['officl'] == "1" and raw[0][0]['cas'] == "0":
-        m['mode'] = "rnk"
+        # m.mode = "rnk"
+        m.mode = 1
     else:
-        m['mode'] = "acc"
-    m['version'] = raw[3][0]['version']
-    m['map_used'] = raw[3][0]['map']
-    m['length'] = raw[3][0]['time_played']
+        # m.mode = "acc"
+        m.mode = 3
+    m.version = raw[3][0]['version']
+    m.map_used = raw[3][0]['map']
+    m.length = raw[3][0]['time_played']
     # '2014-07-27 01:31:18'
     unaware_date = datetime.strptime(raw[3][0]['mdt'], '%Y-%m-%d %H:%M:%S')
-    m['date'] = utc.localize(unaware_date)
+    m.date = utc.localize(unaware_date)
     pitems = {}
     for p in raw[1]:
         items = []
@@ -65,42 +72,39 @@ def single_match(raw, mid):
             if p['slot_' + str(item)]:
                 items.append(int(p['slot_' + str(item)]))
         pitems[p['account_id']] = items
-    players = []
     for p in raw[2]:
         if p['account_id'] not in pitems:
             pitems[p['account_id']] = []
-        players.append({
-            'id': int(p['account_id']),
-            'nickname': p['nickname'],
-            'clan_id': int(p['clan_id']),
-            'hero_id': int(p['hero_id']),
-            'position': int(p['position']),
-            'items': pitems[p['account_id']],
-            'team': int(p['team']),
-            'level': int(p['level']),
-            'win': bool(int(p['wins'])),
-            'concedes': int(p['concedes']),
-            'concedevotes': int(p['concedevotes']),
-            'buybacks': int(p['buybacks']),
-            'discos': int(p['discos']),
-            'kicked': int(p['kicked']),
-            'mmr_change': float(p['amm_team_rating']),
-            'herodmg': int(p['herodmg']),
-            'kills': int(p['herokills']),
-            'assists': int(p['heroassists']),
-            'deaths': int(p['deaths']),
-            'kdr': div(p['herokills'], p['deaths']),
-            'goldlost2death': int(p['goldlost2death']),
-            'secs_dead': int(p['secs_dead']),
-            'cs': int(p['teamcreepkills']) + int(p['neutralcreepkills']),
-            'bdmg': p['bdmg'],
-            'denies': p['denies'],
-            'gpm': divmin(p['gold'], m['length']),
-            'xpm': divmin(p['exp'], m['length']),
-            'apm': divmin(p['actions'], m['length']),
-            'consumables': int(p['consumables']),
-            'wards': int(p['wards'])
-        })
-
-    m['players'] = players
+        m.players.append(PlayerMatch(
+            player_id=int(p['account_id']),
+            nickname=p['nickname'],
+            clan_id=int(p['clan_id']),
+            hero_id=int(p['hero_id']),
+            position=int(p['position']),
+            items=pitems[p['account_id']],
+            team=int(p['team']),
+            level=int(p['level']),
+            win=bool(int(p['wins'])),
+            concedes=int(p['concedes']),
+            concedevotes=int(p['concedevotes']),
+            buybacks=int(p['buybacks']),
+            discos=int(p['discos']),
+            kicked=int(p['kicked']),
+            mmr_change=float(p['amm_team_rating']),
+            herodmg=int(p['herodmg']),
+            kills=int(p['herokills']),
+            assists=int(p['heroassists']),
+            deaths=int(p['deaths']),
+            kdr=div(p['herokills'], p['deaths']),
+            goldlost2death=int(p['goldlost2death']),
+            secs_dead=int(p['secs_dead']),
+            cs=int(p['teamcreepkills']) + int(p['neutralcreepkills']),
+            bdmg=p['bdmg'],
+            denies=p['denies'],
+            gpm=divmin(p['gold'], m.length),
+            xpm=divmin(p['exp'], m.length),
+            apm=divmin(p['actions'], m.length),
+            consumables=int(p['consumables']),
+            wards=int(p['wards'])
+        ))
     return m

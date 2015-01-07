1f59c2a3eb6ef1c1c2d4a935bdef9abd31ee986e
cooper <scttcper@gmail.com>
2015-01-07 00:51:55 -0800

    no touch

--- www/gulp/build.js
+++ www/gulp/build.js
@@ -118,11 +118,6 @@ gulp.task('html', ['wiredep', 'injector:css', 'injector:js', 'partials'], functi
 
 gulp.task('images', function () {
   return gulp.src('src/assets/images/**/*')
-    .pipe($.cache($.imagemin({
-      optimizationLevel: 3,
-      progressive: true,
-      interlaced: true
-    })))
     .pipe(gulp.dest('dist/assets/images/'));
 });
 
